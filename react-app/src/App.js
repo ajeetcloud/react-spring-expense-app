13f78d76f1ff038a57a52ed5304db772162cff04
Ajeet Singh <44796715+ajeetcloud@users.noreply.github.com>
2022-07-13 01:42:53 +0530

    Added expense form

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,15 +1,24 @@
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
+import {useState} from "react";
+
+const staticExpenses = [
+    {id: 'e1', title: 'Car Insurance', amount: '100.25', date: new Date(2022, 5, 20)},
+    {id: 'e2', title: 'Rent', amount: '1000', date: new Date(2022, 10, 1)},
+    {id: 'e3', title: 'Broadband', amount: '50.75', date: new Date(2022, 7, 10)},
+];
 
 const App = () => {
-    const expenses = [
-        {title: 'Car Insurance', amount: '100.25', date: new Date(2022, 5, 20)},
-        {title: 'Rent', amount: '1000', date: new Date(2022, 10, 1)},
-        {title: 'Broadband', amount: '50.75', date: new Date(2022, 7, 10)},
-    ];
 
-    const addExpenseHandler = (newForm) => {
-        console.log(newForm)
+    const [expenses, setExpenses] = useState(staticExpenses);
+
+    const addExpenseHandler = (expense) => {
+        console.log(expense)
+        //setExpenses([expense, ...expenses]);
+        // The better way, as it depends on previous state.
+        setExpenses(prevExpenses => {
+            return [expense, ...prevExpenses];
+        });
     }
 
     return (
--- react-app/src/components/Expenses/Expenses.js
+++ react-app/src/components/Expenses/Expenses.js
@@ -1,25 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
 import './Expenses.css';
+import ExpenseFilter from "./ExpenseFilter";
 
 const Expenses = (props) => {
+    const [filterYear, setFilterYear] = useState('2020');
+
+    const filterChangeHandler = (selectedYear) => {
+        setFilterYear(selectedYear);
+    };
+
     return (
-        <Card className="expenses">
-            <ExpenseItem
-                title={props.items[0].title}
-                amount={props.items[0].amount}
-                date={props.items[0].date}/>
-            <ExpenseItem
-                title={props.items[1].title}
-                amount={props.items[1].amount}
-                date={props.items[1].date}/>
-            <ExpenseItem
-                title={props.items[2].title}
-                amount={props.items[2].amount}
-                date={props.items[2].date}/>
-        </Card>
+        <div>
+            <Card className="expenses">
+                <ExpenseFilter selected={filterYear} onYearChange={filterChangeHandler}></ExpenseFilter>
+                {props.items.map(expense =>
+                    <ExpenseItem
+                        key={expense.id}
+                        title={expense.title}
+                        amount={expense.amount}
+                        date={expense.date}
+                    />
+                )}
+            </Card>
+        </div>
     );
 }
 
